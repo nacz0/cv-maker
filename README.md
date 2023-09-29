79c47080870389559d6faa85e5ee3255a133a88c
nacz0 <natmach2004@gmail.com>
2023-09-29 21:06:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CV-Creator
+
+An application for creating a Curriculum vitae (resume)
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className=" flex items-center justify-center">
+      <div>Cv-maker</div>
+    </main>
+  );
+}
+
+export default App;
