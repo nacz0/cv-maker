caf12ac861e296e280c21430ea872862e27fbdb9
nacz0 <natmach2004@gmail.com>
2023-10-05 19:30:09 +0200

    correct shape for schema

--- src/schemas/cvSchema.ts
+++ src/schemas/cvSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const monthRegex = /^\d{4}-(0[1-9]|1[0-2])$/; // yyyy-mm
 const phoneRegex = /^(\+\d{2}\s\d{3}\s\d{3}\s\d{3}|\d{3}\s\d{3}\s\d{3}|\d{9})$/; // "+xx xxx xxx xxx", "xxx xxx xxx" and "xxxxxxxxx" are valid phone numbers
-const personalInfoSchema = z.object({
+export const personalInfoSchema = z.object({
   image: z.string().nullish(), //base64
   name: z.string().nonempty(),
   surname: z.string().nonempty(),
@@ -14,39 +14,47 @@ const personalInfoSchema = z.object({
   dateOfBirth: z.date().nullish(),
 });
 
-const educationSchema = z.object({
-  level: z.string().nonempty(),
-  school: z.string().nonempty(),
-  specialization: z.string().nonempty(),
-  startDate: z.string().regex(monthRegex, {
-    message: "Invalid date",
-  }),
-  endDate: z
-    .string()
-    .regex(monthRegex, {
-      message: "Invalid date",
+export const educationSchema = z.object({
+  education: z.array(
+    z.object({
+      level: z.string().nonempty(),
+      school: z.string().nonempty(),
+      specialization: z.string().nonempty(),
+      startDate: z.string().regex(monthRegex, {
+        message: "Invalid date",
+      }),
+      endDate: z
+        .string()
+        .regex(monthRegex, {
+          message: "Invalid date",
+        })
+        .nullish(),
+      description: z.string().nullish(),
     })
-    .nullish(),
-  description: z.string().nullish(),
+  ),
 });
 
-const experienceSchema = z.object({
-  company: z.string().nonempty(),
-  position: z.string().nonempty(),
-  localization: z.string().nonempty(),
-  startDate: z.string().regex(monthRegex, {
-    message: "Invalid date",
-  }),
-  endDate: z
-    .string()
-    .regex(monthRegex, {
-      message: "Invalid date",
+export const experienceSchema = z.object({
+  experience: z.array(
+    z.object({
+      company: z.string().nonempty(),
+      position: z.string().nonempty(),
+      localization: z.string().nonempty(),
+      startDate: z.string().regex(monthRegex, {
+        message: "Invalid date",
+      }),
+      endDate: z
+        .string()
+        .regex(monthRegex, {
+          message: "Invalid date",
+        })
+        .nullish(),
+      description: z.string().nullish(),
     })
-    .nullish(),
-  description: z.string().nullish(),
+  ),
 });
 
-const level = z.enum([
+export const level = z.enum([
   "podstawowy",
   "średni",
   "średniozaawansowany",
@@ -54,12 +62,16 @@ const level = z.enum([
   "ojczysty",
 ]);
 
-const languageSchema = z.object({
-  language: z.string().nonempty(),
-  level: level,
+export const languageSchema = z.object({
+  languages: z.array(
+    z.object({
+      language: z.string().nonempty(),
+      level: level,
+    })
+  ),
 });
 
-const consentForPersonalDataProcessingSchema = z
+export const consentForPersonalDataProcessingSchema = z
   .object({
     text: z.string().optional(),
     company: z.string().nonempty(),
@@ -79,23 +91,32 @@ const consentForPersonalDataProcessingSchema = z
     }
   });
 
-const certificateSchema = z.object({
-  name: z.string().nonempty(),
-  organization: z.string().nonempty(),
-  date: z.string().regex(monthRegex, {
-    message: "Invalid date",
-  }),
-  description: z.string().nullish(),
+export const certificateSchema = z.object({
+  certificates: z.array(
+    z.object({
+      name: z.string().nonempty(),
+      organization: z.string().nonempty(),
+      date: z.string().regex(monthRegex, {
+        message: "Invalid date",
+      }),
+      description: z.string().nullish(),
+    })
+  ),
 });
 
-export const cvSchema = z.object({
+export const aboutSchema = z.object({
+  text: z.string().max(1000).nullish(),
+});
+export const CVSchema = z.object({
   personalInfo: personalInfoSchema,
-  about: z.string().max(1000).nullish(),
-  education: z.array(educationSchema),
-  experience: z.array(experienceSchema),
-  languages: z.array(languageSchema),
+  education: educationSchema,
+  experience: experienceSchema,
+  certificates: certificateSchema,
+  languages: languageSchema,
   skills: z.array(z.string().nonempty()),
-  hobbys: z.array(z.string().nonempty()),
-  certificates: z.array(certificateSchema),
+  hobbies: z.array(z.string().nonempty()),
+  about: aboutSchema,
   consentForPersonalDataProcessing: consentForPersonalDataProcessingSchema,
 });
+
+export type CV = z.infer<typeof CVSchema>;
