6137fdeb93259e680060ee3fb7587972e711c37a
nacz0 <natmach2004@gmail.com>
2023-10-05 19:32:27 +0200

    added additional paths to router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Certificates } from "./components/Certificates";
+import { Creator } from "./components/Creator";
+import { Education } from "./components/Education";
+import { Experience } from "./components/Experience";
+import { Landing } from "./components/Landing";
+import { Languages } from "./components/Languages";
+import { Personal } from "./components/Personal";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+  },
+  {
+    path: "/creator",
+    element: <Creator />,
+    children: [
+      {
+        path: "/creator/personal",
+        element: <Personal />,
+      },
+      {
+        path: "/creator/education",
+        element: <Education />,
+      },
+      {
+        path: "/creator/experience",
+        element: <Experience />,
+      },
+      {
+        path: "/creator/certificates",
+        element: <Certificates />,
+      },
+      {
+        path: "/creator/languages",
+        element: <Languages />,
+      },
+    ],
+  },
+]);
 function App() {
-  return (
-    <main className=" flex items-center justify-center">
-      <div>Cv-maker</div>
-    </main>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
