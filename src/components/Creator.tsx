c1301c0af2fd40ff6b165cd5f7623a1eae9e529a
nacz0 <natmach2004@gmail.com>
2023-11-01 22:49:49 +0100

    creator side nav

--- src/components/Creator.tsx
+++ src/components/Creator.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { useCVStore } from "../cvStore";
 import MenuHamburgerIcon from "../svgs/menuHamburger";
+import { CV } from "./CV";
 
 export function Creator() {
   return (
@@ -9,10 +11,32 @@ export function Creator() {
       <div className=" w-1/2 flex justify-center items-center">
         <Outlet />
       </div>
+      <div className="w-1/4">
+        <CVPreview />
+      </div>
     </main>
   );
 }
 
+function CVPreview() {
+  const info = useCVStore((state) => state);
+  console.log(info);
+  return (
+    <div>
+      <CV info={info} />
+    </div>
+  );
+}
+const sections = [
+  { name: "Personal", address: "personal" },
+  { name: "Education", address: "education" },
+  { name: "Experience", address: "experience" },
+  { name: "Certificates", address: "certificates" },
+  { name: "Languages", address: "languages" },
+  { name: "Skills & Hobbies", address: "skills&hobbies" },
+  { name: "About & Consent", address: "about&consent" },
+  { name: "Preview", address: "preview" },
+];
 function Menu() {
   const [open, setOpen] = useState(true);
   return (
@@ -29,6 +53,23 @@ function Menu() {
           >
             <MenuHamburgerIcon />
           </button>
+        </div>
+        <div className="flex flex-col items-center justify-center h-full">
+          {sections.map((section) => {
+            return (
+              <div
+                key={section.address}
+                className="w-full flex justify-center items-center h-16"
+              >
+                <a
+                  href={`/creator/${section.address}`}
+                  className="text-white text-2xl"
+                >
+                  {section.name}
+                </a>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
